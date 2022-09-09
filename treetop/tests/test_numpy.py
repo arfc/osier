deaf6801d7ac2b2fbadaff9b8a2efd14f5340ce4
Sam Dotson <samgdotson@gmail.com>
2022-09-09 10:49:41 -0500

    pep8

--- treetop/tests/test_numpy.py
+++ treetop/tests/test_numpy.py
@@ -5,7 +5,8 @@ Once other functionality has been added to `treetop`, delete this file.
 
 import numpy as np
 
+
 def test_addition():
-    assert(np.add(1,2)==3)
+    assert(np.add(1, 2) == 3)
 
     return
