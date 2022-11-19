df4cd71d488ca773472525b004355a4609fc36be
Sam Dotson <samgdotson@gmail.com>
2022-11-19 15:27:47 -0600

    fixes problem in file

--- osier/equations.py
+++ osier/equations.py
@@ -3,7 +3,7 @@ from abc import ABC, abstractmethod
 class OsierEquation(ABC):
 
     def __init__(self) -> None:
-        
+        pass        
 
     @abstractmethod
     def _do(self, technology_list, X):
