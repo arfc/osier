f34f3b26a29853171960e46dfaaf6ade3b1a0bfc
Sam Dotson <samgdotson@gmail.com>
2023-03-28 13:01:15 -0500

    minor changes

--- osier/models/logic_dispatch.py
+++ osier/models/logic_dispatch.py
@@ -12,7 +12,7 @@ class LogicDispatchModel(OsierModel):
                  *args, **kwargs):
         super().__init__(technology_list=technology_list, 
                          net_demand=net_demand,
-                         **args, **kwargs)
+                         *args, **kwargs)
         
         self.storage_techs = get_storage_techs(self.technology_list)
         self.nonstorage_techs = get_nonstorage_techs(self.technology_list)
@@ -26,26 +26,27 @@ class LogicDispatchModel(OsierModel):
 
     def solve(self):
         covered_demand = self.net_demand.copy()
-        nonstorage_techs = deepcopy(self.nonstorage_techs)
-        nonstorage_techs.sort()
+        self.nonstorage_techs.sort()
 
-        storage_techs = deepcopy(self.storage_techs)
-        storage_techs.sort()
+        self.storage_techs.sort()
 
         for i, v in enumerate(covered_demand):
 
+            # there is unmet demand
             if v > 0:
-                for t in nonstorage_techs:
+                for t in self.nonstorage_techs:
                     power_out = t.power_output(v, 
                                                time_delta=self.time_delta)
                     v -= power_out
-                    print(f"{t.technology_name} output {power_out}")
+                    # print(f"{t.technology_name} output {power_out}")
                     if v <= 0:
                         break
                     else:
                         continue
+            
+            # there is still unmet demand, dispatch storage technologies
             if v > 0:
-                for s in storage_techs:
+                for s in self.storage_techs:
                     power_out = s.power_output(v, 
                                                time_delta=self.time_delta)
                     v -= power_out
@@ -53,9 +54,10 @@ class LogicDispatchModel(OsierModel):
                         break
                     else:
                         continue
-
+            
+            # there is excess energy (e.g., from renewables), store excess
             if v < 0:
-                for s in storage_techs:
+                for s in self.storage_techs:
                     power_in = s.charge(v, 
                                         time_delta=self.time_delta)
                     v += power_in
@@ -64,6 +66,6 @@ class LogicDispatchModel(OsierModel):
                     else:
                         continue
 
-        covered_demand[i] = v
+            covered_demand[i] = v
         
         return covered_demand
